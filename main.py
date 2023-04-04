6d748e81d1b0d3155f5470c482de077f42ef0047
LumieOwO <120320080+LumieOwO@users.noreply.github.com>
2023-04-04 20:50:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import ssl
 import socket
-from threading import Thread
-from multiprocessing import Process
+import threading
+from concurrent.futures import ThreadPoolExecutor
 
 MAX_THREADS = 500
 URL = ""
-
 
 class StressTester:
     def __init__(self, url):
@@ -19,30 +18,37 @@ class StressTester:
             path = '/' + '/'.join(path_parts)
         else:
             path = '/'
-        self.request = f"GET {path} HTTP/1.1\r\nHost: {self.host}\r\n\r\n"
+        self.request = f"GET {path} HTTP/1.1\r\nHost: {self.host}\r\nConnection: keep-alive\r\n\r\n"
+        self.lock = threading.Lock()
+        self.sock_pool = []
 
     def start(self):
-        try:
-            processes = []
-            for i in range(MAX_THREADS):
-                p = Process(target=self.send_requests)
-                processes.append(p)
-                p.start()
+        with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+            while True:
+                executor.submit(self.send_request)
 
-            for p in processes:
-                p.join()
-        except KeyboardInterrupt:
-            print(self.counter)
-            exit()
-    def send_requests(self):
+    def send_request(self):
+        sock = self.get_socket()
+        sock.sendall(self.request.encode('utf-8'))
+        with self.lock:
+            self.counter += 1
+        self.release_socket(sock)
+
+    def get_socket(self):
+        with self.lock:
+            if self.sock_pool:
+                return self.sock_pool.pop()
         sock = self.create_socket()
         sock.connect((self.host, self.port))
-        sock.sendall(self.request.encode('utf-8'))
-        while True:
-            sock.sendall(self.request.encode('utf-8'))
-            self.counter += 1
+        return sock
+
+    def release_socket(self, sock):
+        with self.lock:
+            self.sock_pool.append(sock)
+
     def create_socket(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 16384) 
         sock.settimeout(30)
         if self.protocol == "https":
             return ssl.create_default_context().wrap_socket(sock=sock, server_hostname=self.host)
@@ -64,4 +70,3 @@ class StressTester:
 if __name__ == "__main__":
     stress_tester = StressTester(url=URL)
     stress_tester.start()
-    Process(target=stress_tester.start()).start()
